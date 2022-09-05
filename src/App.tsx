8ed30de2c257383d28d338ce9d333d43359ace3b
vithushjeytharma@gmail.com <vithushjeytharma@gmail.com>
2022-09-05 11:15:36 +0530

    fix: refactor code

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,66 @@
-import Logo from "./assets/react.svg";
 import "./App.css";
-import WorkerBuilder from "./worker/worker-builder";
-import normalWorker from "./worker/normal-worker";
-import worker from "./worker/other-worker";
-import {
-  createWorkerFactory,
-  useWorker as useShopifyWorker,
-} from "@shopify/react-web-worker";
-import { useWorker as useKoaleWorker } from "@koale/useworker";
-
-// init shopify createWorkerFactory
-const createWorker = createWorkerFactory(() => import("./worker/other-worker"));
+import { useEffect, useState } from "react";
+import worker from "./worker/worker-two";
+import useShopifyWorker from "./hooks/useShopifyWorker";
+import useKoalaWorker from "./hooks/useKoalaWorker";
+import useNormalWorker from "./hooks/useNormalWorker";
 
 // complex array constant
-const numbers: Array<number> = [...Array(5000000)].map(
+const nums: Array<number> = [...Array(5000000)].map(
   (e) => ~~(Math.random() * 1000000)
 );
 
-// call normal-worker without using any libs
-const runNormalWorker = () => {
-  // initiate worker
-  const worker = new WorkerBuilder(normalWorker);
-
-  // post message to worker
-  worker.postMessage(numbers);
-
-  // receive message from worker
-  worker.onmessage = (message) => {
-    console.log("worker message: ", message.data);
-  };
-
-  // receive error from worker
-  worker.onerror = (error) => {
-    console.log("worker error: ", error.message);
-  };
-};
+// call function without worker
+const runFunction = () => worker([...nums]);
 
 function App() {
-  // shopify web worker hook
-  const shopifyWorker = useShopifyWorker(createWorker);
-  // call other-worker using shopify web worker
-  const runShopifyWorker = async () => {
-    try {
-      const result = await shopifyWorker.default(numbers);
-      console.log("shopify worker result ", { result });
-    } catch (error) {
-      console.log("error ", error);
-    }
-  };
+  // time state
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
 
-  // koale web worker hook
-  const [koaleWorker] = useKoaleWorker(worker);
-  // call other-worker using shopify web worker
-  const runKoaleWorker = async () => {
-    try {
-      const result = await koaleWorker(numbers);
-      console.log("koale worker result ", { result });
-    } catch (error) {
-      console.log("error ", error);
-    }
-  };
+  // hook to update time state after each second
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
+  // normal woker
+  const [runNormalWorker] = useNormalWorker([...nums]);
+  // shopify worker
+  const [runShopifyWorker] = useShopifyWorker([...nums]);
+  // koala worker
+  const [runKoaleWorker] = useKoalaWorker([...nums]);
 
   return (
-    <div className="app">
-      <img className="logo" alt="react-logo" src={Logo} />
-      <a
-        href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API"
-        target="_blank"
-      >
-        Web worker
-      </a>
-      <button
-        title="Normal Web worker implementation"
-        onClick={runNormalWorker}
-      >
-        Run Normal Worker
-      </button>
-      <button
-        title="Shopify Web worker implementation"
-        onClick={runShopifyWorker}
-      >
-        Run Shopify Worker
-      </button>
-      <button title="Koale Web worker implementation" onClick={runKoaleWorker}>
-        Run Koale Worker
-      </button>
+    <div className="container">
+      <h1>{time}</h1>
+      <div className="wrapper">
+        <button title="Run function without web worker" onClick={runFunction}>
+          Run Function
+        </button>
+        <button
+          title="Normal web worker implementation"
+          onClick={runNormalWorker}
+        >
+          Run Normal Worker
+        </button>
+        <button
+          title="@shopify/react-web-worker web worker implementation"
+          onClick={runShopifyWorker}
+        >
+          Run Shopify Worker
+        </button>
+        <button
+          title="@koale/useworker web worker implementation"
+          onClick={runKoaleWorker}
+        >
+          Run Koale Worker
+        </button>
+      </div>
     </div>
   );
 }
