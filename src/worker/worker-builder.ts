bd9b1ce49baab9a46e4c7ead3ea8296448770039
vithushjeytharma@gmail.com <vithushjeytharma@gmail.com>
2022-09-05 12:07:46 +0530

    fix: change worker builder instance name

--- src/worker/worker-builder.ts
+++ src/worker/worker-builder.ts
@@ -1,7 +1,7 @@
 // normal web worker implementation
 export default class WorkerBuilder extends Worker {
   constructor(worker: () => void) {
-    super(worker);
+    super("");
     const code = worker.toString();
     const blob = new Blob([`(${code})()`]);
     return new Worker(URL.createObjectURL(blob));
